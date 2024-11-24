6ac231abe44a38b41655f3761a11ba997cb8e4d5
Nico Pradas <118016471+Nicopradas@users.noreply.github.com>
2024-11-24 22:10:52 +0100

    Create function getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log('Hello World!');
+
+/* 
+PSEUDOCODE OF GetComputerChoice:
+CREATE A FUNCTION THAT IS NAME GETCOMPUTERCHOICE
+    CREATE A RANDOM NUMBER BETWEEN 0 AND 0.99
+    CREATE VARIABLE "ROCK"
+    CREATE VARIABLE "PAPER"
+    CREATE VARIABLE "SCISSORS"
+    IF RANDOM NUMBER IS HIGHER OR EQUAL TO 0 AND LOWER THAN 0.33 RETURNS VARIABLE ROCK
+    IF RANDOM NUMBER IS HIGHER OR EQUAL TO 0.33 OR LOWER THAN 0.66 RETURNS VARIABLE PAPER
+    IF RANDOM NUMBER IS ANYTHING ELSE, RETURNS VARIABLE SCISSORS
+*/
+
+function getComputerChoice(){
+    let randomNumber = Math.random();
+    const ROCK = 'ROCK';
+    const PAPER = 'PAPER';
+    const SCISSORS = 'SCISSORS';
+
+    if (randomNumber>=0 && randomNumber<0.33){
+        return ROCK;
+    } else if(randomNumber>=0.33 && randomNumber<0.66){
+        return PAPER;
+    } else{
+        return SCISSORS;
+    }
+}
+
+
+
+console.log(getComputerChoice());
