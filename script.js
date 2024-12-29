9d3c9df220887a6c8f3cda81210d76366bc688bd
Nico Pradas <118016471+Nicopradas@users.noreply.github.com>
2024-12-29 22:12:39 +0100

    Modify playRound()

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ const scissors = document.querySelector('#scissors');
 let humanScore = 0;
 let computerScore = 0;
 
+rock.addEventListener('click',()=>{
+    playRound(rock.id,getComputerChoice());
+
+});
+paper.addEventListener('click',()=>{
+    playRound(paper.id,getComputerChoice());
+
+});
+scissors.addEventListener('click',()=>{
+    playRound(scissors.id,getComputerChoice());
+});
+
 
 
 
@@ -13,35 +25,34 @@ function getComputerChoice(){
     
 
     if (randomNumber>=0 && randomNumber<0.33){
-        return ROCK;
+        return 'rock';
     } else if(randomNumber>=0.33 && randomNumber<0.66){
-        return PAPER;
+        return 'paper';
     } else{
-        return SCISSORS;
+        return 'scissors';
     }
     
 }
 
 
 function playRound(humanSelection, computerSelection){
-    let humanSelectionFixed = humanSelection.toUpperCase();
-    console.log(humanSelectionFixed);
-    if ((humanSelectionFixed === 'paper') && (computerSelection === ROCK) ){
+    
+    if ((humanSelection === 'paper') && (computerSelection === 'rock') ){
         humanScore++;
-        console.log('You won! '+humanSelectionFixed+' beats '+computerSelection);
-    } else if ((humanSelectionFixed === 'scissors') && (computerSelection === PAPER) ){
+        console.log('You won! '+humanSelection+' beats '+computerSelection);
+    } else if ((humanSelection === 'scissors') && (computerSelection === 'paper') ){
         humanScore++;
-        console.log('You won! '+humanSelectionFixed+' beats '+computerSelection);
-    } else if ((humanSelectionFixed === 'rock') && (computerSelection === SCISSORS) ){
+        console.log('You won! '+humanSelection+' beats '+computerSelection);
+    } else if ((humanSelection === 'rock') && (computerSelection === 'scissors') ){
         humanScore++;
-        console.log('You won! '+humanSelectionFixed+' beats '+computerSelection);
-    } else if (humanSelectionFixed === computerSelection){
+        console.log('You won! '+humanSelection+' beats '+computerSelection);
+    } else if (humanSelection === computerSelection){
         console.log('It is a tie!');
-        console.log(humanSelectionFixed+' is the same as '+computerSelection);
+        console.log(humanSelection+' is the same as '+computerSelection);
 
     }  else {
         computerScore++;
-        console.log('You lose! '+computerSelection+' beats '+humanSelectionFixed);
+        console.log('You lose! '+computerSelection+' beats '+humanSelection);
     }
 
     
@@ -51,11 +62,5 @@ function playRound(humanSelection, computerSelection){
 
     
 
-    playRound(getHumanChoice(), getComputerChoice());
-
-
-
-
-
-
+   
 
